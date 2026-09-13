0084c79ee11f0e5fc4dc8c108f2db1e630657451
zakisafri719-beep <zakisafri719@gmail.com>
2026-09-13 19:08:07 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ai-chat-v1';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(name => name !== CACHE_NAME)
+             .map(name => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
